3395db8aba9bdb2fd230e5f75e0bcfce626e9c10
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2018-01-12 22:04:17 -0500

    add test for paying the splitter

--- test/splitter.js
+++ test/splitter.js
@@ -1,4 +1,5 @@
 const Splitter = artifacts.require('Splitter');
+const getBalance = require('./util/getBalance');
 
 contract('Splitter', ([ owner, ...accounts ]) => {
   let splitter;
@@ -9,6 +10,20 @@ contract('Splitter', ([ owner, ...accounts ]) => {
 
   it('is deployed', () => {
     assert.strictEqual(typeof splitter.address, 'string');
+  });
+
+  it('can be paid', async () => {
+    const balanceBefore = await getBalance(splitter.address);
+    // TODO: figure out why this reverts
+    await splitter.sendTransaction({ value: 10 });
+    const balanceAfter = await getBalance(splitter.address);
+
+    assert.strictEqual(balanceAfter.sub(balanceBefore).valueOf(), '10');
+  });
+
+  describe('#payout', () => {
+    it('splits it evenly with any number of payees');
+    it('always ends up with 0 balance, regardless of the leftover');
   });
 
 });
