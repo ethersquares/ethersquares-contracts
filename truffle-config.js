85af66c7ffbc08e353a438b583c9522bc5a624a2
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2018-01-13 11:32:01 -0500

    add the hdwallet provider and a config for kovan

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,12 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(process.env.MNEMONIC, 'wss://mainnet.infura.io/ws');
+      },
+      network_id: '42' // Match any network id
+    }
+  }
 };
