c9276667f54746512f68ab32fb18cecdc26cd512
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2018-01-13 11:43:31 -0500

    add the hdwallet provider and a config for kovan/mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -4,9 +4,15 @@ module.exports = {
   networks: {
     kovan: {
       provider: function () {
-        return new HDWalletProvider(process.env.MNEMONIC, 'wss://mainnet.infura.io/ws');
+        return new HDWalletProvider(process.env.MNEMONIC_KOVAN, `https://kovan.infura.io/${process.env.INFURA_API_KEY}`);
       },
       network_id: '42' // Match any network id
+    },
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(process.env.MNEMONIC_MAINNET, `https://mainnet.infura.io/${process.env.INFURA_API_KEY}`);
+      },
+      network_id: '1' // Match any network id
     }
   }
 };
