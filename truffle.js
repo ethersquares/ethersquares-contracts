67328b25f97a24a47918d587223a5106e3d4cc53
Moody <moody.salem@gmail.com>
2018-01-17 17:51:20 -0500

    add gas prices

--- truffle.js
+++ truffle.js
@@ -7,13 +7,14 @@ module.exports = {
         return new HDWalletProvider(process.env.MNEMONIC_KOVAN, `https://kovan.infura.io/${process.env.INFURA_API_KEY}`);
       },
       network_id: '42', // Match any network id
-      gasPrice: 100
+      gasPrice: 20
     },
     mainnet: {
       provider: function () {
         return new HDWalletProvider(process.env.MNEMONIC_MAINNET, `https://mainnet.infura.io/${process.env.INFURA_API_KEY}`);
       },
-      network_id: '1' // Match any network id
+      network_id: '1', // Match any network id
+      gasPrice: 51
     }
   }
 };
